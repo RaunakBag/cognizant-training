b1265cc6728c5e3d100b11b87d021531ee222a9e
Aniket Jha <imanik8@hotmail.com>
2021-03-10 12:55:28 +0530

    JavaScript Fundamentals Completed

---
+++ UI & Scripting (Week 3&4)/complete-javascript-course-master/01-Fundamentals-Part-1/Exercise/script.js
@@ -0,0 +1,119 @@
+// // // // /**********************************************/
+
+// // // // //Values and Variables
+
+// // // // // let country = "India";
+// // // // // let continent = "Asia";
+// // // let population = 1360000000;
+// // // // // console.log(country);
+// // // // // console.log(continent);
+// // // //console.log(population);
+
+// // // // // //Data Types
+
+// // // // // let isIsland = false;
+// // // // // let langauge;
+// // // // // console.log(typeof isIsland);
+// // // // // console.log(typeof population);
+// // // // // console.log(typeof country);
+// // // // // console.log(typeof langauge);
+
+// // // // //let, const and var
+
+// // // language = "hindi";
+// // // const country = "India";
+// // // const continent = "Asia";
+// // // const isIsland = true;
+// // // //isIsland = false;
+// // // // console.log(country, continent, isIsland);
+
+// // // // //Basic Operators
+// // // // console.log(population / 2);
+// // // // console.log(population++);
+// // // const populationFinland = 60000000;
+// // // // console.log(population > populationFinland);
+// // // const averagePop = 330000000;
+// // // // console.log(population < averagePop);
+// // // // const description =
+// // // //   country +
+// // // //   " is in " +
+// // // //   continent +
+// // // //   ", and its " +
+// // // //   population +
+// // // //   " people speak hindi.";
+
+// // // // console.log(description);
+
+// // // //population = 130000000;
+// // // //population = 1300000000;
+
+// // // //Strings and Template Literals
+// // // const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+// // // console.log(description);
+
+// // // if (population > averagePop) {
+// // //   console.log(`India's population is above average.`);
+// // // } else {
+// // //   console.log(`India's population is 22 million below average`);
+// // // }
+
+// // // population = 130000000;
+// // // population = 1300000000;
+
+// // //Type Conversion and Type Coercion
+// // console.log("9" - "5");
+// // console.log("19" - "13" + "17");
+// // console.log("19" - "13" + 17);
+// // console.log("123" < 57);
+// // console.log(5 + 6 + "4" + 9 - 4 - 2);
+
+// //Equality Operator == or ===
+// numNeighbours = Number(
+//   prompt("How many neighbour countries does your country have?")
+// );
+// if (numNeighbours === 1) {
+//   alert("Only 1 border!");
+// } else if (numNeighbours > 1) {
+//   alert("More than 1 border");
+// } else {
+//   alert("No Borders");
+// }
+
+// Logical Operators //
+// if (language === "english" && population < 50 && !isIsland) {
+//   console.log("You should live in Portugal :)");
+// } else {
+//   console.log("Portugal does not meet the criteria :(");
+// }
+
+//Switch Stactement
+
+// const lang = hindi;
+
+// switch (language) {
+//   case "chinese":
+//   case "mandarin":
+//     console.log("MOST number of native speakers!");
+//     break;
+//   case "spanish":
+//     console.log("2nd place in number of native speakers");
+//     break;
+//   case "english":
+//     console.log("3rd place");
+//     break;
+//   case "hindi":
+//     console.log("Number 4");
+//     break;
+//   case "arabic":
+//     console.log("5th most spoken language");
+//     break;
+//   default:
+//     console.log("Great language too :D");
+// }
+
+//Ternary Operator
+// const country = "India";
+// const population = 36;
+// console.log(
+//   `${country}'s population is ${population > 33 ? "above" : "below"} average`
+// );
